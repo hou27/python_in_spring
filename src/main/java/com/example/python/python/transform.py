3accfbe0940adbc1dcd567d98915894b0a26b5c9
hou27 <ataj125@gmail.com>
2023-11-04 17:32:08 +0900

    feat: pickle dump 시 버전을 낮춰 시도
    pickle protocol 2로 dump 후 load하니 정상적으로 load됨.
    그러나 scikit learn 패키지 문제로 실행이 안되는 상황

--- src/main/java/com/example/python/python/transform.py
+++ src/main/java/com/example/python/python/transform.py
@@ -4,7 +4,7 @@ def transform(dataArray_str):
     print(dataArray_str)
     dataArray = list(map(float, dataArray_str[1:-1].split(',')))
     print(dataArray)
-    with open("src/main/java/com/example/python/python/scaler/scaler1020.pkl", "rb") as f:
+    with open("src/main/java/com/example/python/python/scaler/scaler1020_protocol2.pkl", "rb") as f:
         scaler = pickle.load(f)
 
         return scaler.transform(dataArray)
